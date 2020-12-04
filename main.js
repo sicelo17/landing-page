11aa4531c823ac52d2ae4ac0d8059662c759ead9
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2020-12-04 02:49:10 +0200

    animated line

--- main.js
+++ main.js
@@ -15,11 +15,29 @@ function marg1(){
     setTimeout(marg2, 8000);
 }
 setTimeout(marg1, 8000);
+setInterval(line, 7000);
+setInterval(line2, 8700)
 
 function slid(){
     var s = document.querySelectorAll('.slid');
     var i;
     for (i=0; i<s; i++){
         s[i].style.transform='scale(.8)';
+    }
+}
+
+function line(){
+    var l = document.querySelectorAll('.line');
+    var i;
+    for (i=0; i<l.length; i++){
+        l[i].style.transform = 'scale(0)';
+    }
+}
+
+function line2(){
+    var l = document.querySelectorAll('.line');
+    var i;
+    for (i=0; i<l.length; i++){
+        l[i].style.transform = 'scale(1)';
     }
 }
